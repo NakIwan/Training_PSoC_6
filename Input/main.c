8988e42b0e00653912275ee2173d4c5ffdea1d1d
NakIwan <ridwanbahari236@gmail.com>
2024-01-26 11:11:42 +0700

    Commiiii

--- Input/main.c
+++ Input/main.c
@@ -1,87 +1,8 @@
-/*******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the Empty Application Example
-*              for ModusToolbox.
-*
-* Related Document: See README.md
-*
-*
-********************************************************************************
-* Copyright 2021-2023, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
-
-/*******************************************************************************
-* Header Files
-*******************************************************************************/
 #include "cyhal.h"
 #include "cybsp.h"
 #include "cy_pdl.h"
 #include "cy_retarget_io.h"
 
-/*******************************************************************************
-* Macros
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Global Variables
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Function Prototypes
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Function Definitions
-*******************************************************************************/
-
-/*******************************************************************************
-* Function Name: main
-********************************************************************************
-* Summary:
-* This is the main function for CPU. It...
-*    1.
-*    2.
-*
-* Parameters:
-*  void
-*
-* Return:
-*  int
-*
-*******************************************************************************/
     uint32_t currentLED;
     uint32_t longPressTime = 1000, shortPressTime = 500;
     uint32_t pressTime, releaseTime;//, timePressLimit;
@@ -173,7 +94,7 @@ int main(void)
 	}
 
 	/* Initialize the user button */
-	result = cyhal_gpio_init(P0_4, CYHAL_GPIO_DIR_INPUT, CYHAL_GPIO_DRIVE_PULLUP, 1U);
+	result = cyhal_gpio_init(CYBSP_USER_BTN1, CYHAL_GPIO_DIR_INPUT, CYHAL_GPIO_DRIVE_PULLUP, 1U);
 	/* GPIO init failed. Stop program execution */
 	if (result != CY_RSLT_SUCCESS)
 	{
@@ -195,15 +116,6 @@ int main(void)
 
     for (;;)
     {
-    	//Button is active low
-    	/*if(!cyhal_gpio_read(P0_4)){
-    		cyhal_gpio_write(P6_3, CYBSP_LED_STATE_ON);
-    	}
-    	else{
-    		cyhal_gpio_write(P6_3, CYBSP_LED_STATE_OFF);
-    	}
-    	//Cy_SysTick_GetClockSource();*/
-
     	currentState =! cyhal_gpio_read(P0_4);
 
     	if(lastState == 0 && currentState == 1){
